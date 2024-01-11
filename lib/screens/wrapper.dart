688fb829a91aaa1caf4f14c11bcf19623d66e10c
O'Neil <owedjangnon@gmail.com>
2024-01-11 15:30:27 +0100

    page apropos
    modif de la page à propos et d'une ligne de la page wrapper.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -4,20 +4,16 @@ import 'package:vhireapp/models/user.dart';
 import 'package:vhireapp/screens/authentication/login_page.dart';
 import 'package:vhireapp/screens/home/home/await_get_user.dart';
 
-import 'home/home/Profile.dart';
-
-
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final AuthenticatedUser? user = Provider.of<AuthenticatedUser?>(context);
 
     // return either signIn or home page
-    if(user==null) {
-      return const Profile();
+    if (user == null) {
+      return const LoginPage();
     } else {
       return GetUpdateUserBeforeHome(user: user);
     }
